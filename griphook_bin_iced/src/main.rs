7b4c6ffa9a50f0a66e1254d1e7467bc27291917c
Manuel Simon <simon.manuel27@gmail.com>
2026-08-28 16:54:45 +0200

    Change the application window title to "Griphook".

--- griphook_bin_iced/src/main.rs
+++ griphook_bin_iced/src/main.rs
@@ -4,7 +4,7 @@ use griphook::app::App;
 
 fn main() -> iced::Result {
     iced::application(App::new, App::update, App::view)
-        .title("Image Renamer")
+        .title("Griphook")
         .theme(App::theme)
         .subscription(App::subscription)
         .run()
